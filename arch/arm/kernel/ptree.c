d0fd5c96987108b3fe5a4d60543939f806a7661e
Seunggyu Lee <seunggyu.lee0619@gmail.com>
2017-03-20 01:23:23 +0900

    Update ptree.c

--- arch/arm/kernel/ptree.c
+++ arch/arm/kernel/ptree.c
@@ -1,7 +1,7 @@
 #include <linux/linkage.h>
 #include <linux/kernel.h>
 
-asmlinkage int sys_ptree(void)
+asmlinkage int sys_ptree(struct prinfo *buf, int *nr)
 {
 	printk(KERN_EMERG "HELLOWORLD\n");
 	return 1;
