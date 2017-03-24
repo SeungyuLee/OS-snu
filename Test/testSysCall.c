f77cf2e8b576e99d0bb004a3d9b715984f7012ce
SeungyuLee <seunggyu.lee0619@gmail.com>
2017-03-24 14:26:37 +0900

    EFAULT_handling and skeleton codes added

--- Test/testSysCall.c
+++ Test/testSysCall.c
@@ -1,7 +1,12 @@
 #define __NR_ptree 380
+#include <unistd.h>
+#include <stdio.h>
+#include <prinfo.h>
+__syscall2(int, ptree, struct *, prinfo, int *, nr)
 
 int main()
 {
-	syscall(__NR__ptree);
+//	int result = ptree( , );
+	printf("user hello");
 	return 0;
 }
