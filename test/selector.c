2d872808c7418c75c82293f45501e12762d59109
Lee-nayoung <nylee1122@gmail.com>
2017-04-09 15:28:32 +0900

    add selector.c

---
+++ test/selector.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/syscalls.h>
+
+#define SYSCALL_ROTLOCK_WRITE 382
+#define SYSCALL_ROTUNLOCK_WRITE 384
+
+static volatile int keepRunning = 1;
+
+void intHandler(int dummy) {
+	keepRunning = 0;
+}
+
+int main(int argc){
+	FILE *p;
+	int inputInt = argc;
+	
+	signal(SIGINT, intHandler);
+	
+	while(keepRunning){
+		if(syscall(SYSCALL_ROTLOCK_WRITE, 0, 180) == 0){
+			fprintf(fp, "%d", inputInt);
+			printf("select: %d\n", inputInt);
+			inputInt++;
+			fclose(fp);
+			syscall(SYSCALL_ROTUNNLOCK_WRITE, 0, 180);
+		}
+	}
+
+	return 0;
+}
