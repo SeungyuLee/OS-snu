41c256df129f3300f32e89221241f7e13d1f3656
SeungyuLee <seunggyu.lee0619@gmail.com>
2017-04-09 16:41:30 +0900

    minor changes

--- arch/arm/kernel/readwritelock.c
+++ arch/arm/kernel/readwritelock.c
@@ -2,29 +2,30 @@
 #include<linux/sched.h>
 #include<asm/uaccess.h>
 #include<linux/slab.h>
+#include<linux/rotation.h>
 
 enum LockType {
-	kInvalid = 0;
-	kRead = 1;
-	kWrite = 2;
-}
+	kInvalid = 0,
+	kRead = 1,
+	kWrite = 2
+};
 
 struct lock_struct {
 	int* degreeStart, degreeEnd;
 	enum lockType *type;
-	int* count;
+	atomic_t* count;
 	struct list_head list;
-}
+};
 
 static LIST_HEAD(current_lock_list);
 static LIST_HEAD(waiting_lock_list);
 
 bool canLock(struct lock_struct *info) {
-	printk(KERN_EMEG "trying lock : %d %d %d",info->degreeStart,info->degreeEnd,info->type);
+	printk(KERN_DEBUG "trying lock : %d %d %d",info->degreeStart,info->degreeEnd,info->type);
 	// 현재 degree와 비교 작업 먼저
-	list_for_each(head, current_lock_list) {
+	list_for_each_entry_safe(head, current_lock_list) {
 		struct lock_struct *lock = list_entry(head,struct lock_struct,list);
-		printk(KERN_EMEG "current lock : %d %d %d",lock->degreeStart, lock->degreeEnd,lock->type);
+		printk(KERN_DEBUG "current lock : %d %d %d",lock->degreeStart, lock->degreeEnd,lock->type);
 		if ((lock->degreeStart <= info->degreeStart && lock->degreeEnd >= info->degreeStart) ||
 				(lock->degreeStart <= info->degreeEnd && lock->degreeEnd >= info->degreeEnd)) {
 			if (info->type == kWrite || lock->type == kWrite) {
@@ -32,10 +33,10 @@ bool canLock(struct lock_struct *info) {
 			}
 		}
 	}
-	if (info.type == kRead) {
-		list_for_each(head, waiting_lock_list) {
+	if (info->type == kRead) {
+		list_for_each_entry_safe(head, waiting_lock_list) {
 			struct lock_struct *lock = list_entry(head,struct lock_struct,list);
-			printk(KERN_EMEG "wating write lock : %d %d %d",lock->degreeStart, lock->degreeEnd, lock->type);
+			printk(KERN_DEBUG "wating write lock : %d %d %d",lock->degreeStart, lock->degreeEnd, lock->type);
 			if(lock->type == kWrite) {
 				if ((lock->degreeStart <= info->degreeStart && lock->degreeEnd >= info->degreeStart) ||
 						(lock->degreeStart <= info->degreeEnd && lock->degreeEnd >= info->degreeEnd)) {
@@ -52,21 +53,20 @@ bool canLock(struct lock_struct *info) {
 
 void insertIntoList(struct list_head *list, int degree, int range, enum LockType type) {
 	// 락해주고
-	list_for_each(head, list) {
+	list_for_each_entry_safe(head, list) {
 		struct lock_struct *lock = list_entry(head,struct lock_struct,list);
 		if(lock->degreeStart == degree - range && lock->degreeEnd == degree + range && lock->type == type) { 
-			lock->count += 1; // count 락을 따로 해줘야 할 수도 있음, 근데 위에 락에 안걸리고 여기로 오는 경우가 생각이 안남
-			//락풀고
+			atomic_inc(lock->count); //락풀고
 			return;
 		}
 	}
 	// 락풀고
 	struct lock_struct *new_lock;
 	new_lock = kmalloc(sizeof(*read_lock), GFP_KERNEL);
-	new_lock->degreeStart = dgree - range;
+	new_lock->degreeStart = degree - range;
 	new_lock->degreeEnd = degree + range;
-	new_lock->type = type;
-	new_lock->count = 1;
+	new_lock->type = &type;
+	atomic_set(new_lock->count, 1);
 	INIT_LIST_HEAD(&new_lock->list);
 	// 락해주고
 	list_add(&new_lock,&list);
@@ -76,12 +76,12 @@ void insertIntoList(struct list_head *list, int degree, int range, enum LockType
 int deleteFromList(struct list_head *list, int degree, int range, enum LockType type) {
 	// 같은걸 지우지 않도록 락거는 시점이 중요
 	// 락해주고
-	list_for_each(head, list) {
+	list_for_each_entry_safe(head, list) {
 		struct lock_struct *lock = list_entry(head,struct lock_struct,list);
 		if(lock->degreeStart == degree - range && lock->degreeEnd == degree + range, lock->type == type) {
 			// 위와 마찬가지 이유로 count 락 걸지 않음
-			if(lock->count > 1) {
-				lock->count -= 1;
+			if(atomic_read(lock->count) > 1) {
+				atomic_dec(lock->count);
 			}else {
 				list_del(&lock->list);
 				kfree(lock);
@@ -107,7 +107,7 @@ asmlinkage int sys_rotlock_read(int degree, int range) {
 	return lockProcess(degree,range,kRead);
 }
 
-asmlinkage int sys_rotlock_write(int drgree, int range) {
+asmlinkage int sys_rotlock_write(int degree, int range) {
 	return lockProcess(degree,range,kWrite);
 }
 
