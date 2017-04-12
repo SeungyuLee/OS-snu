5d257d0941e4976705a3290e2ee3eb425890f34d
SeungyuLee <seunggyu.lee0619@gmail.com>
2017-04-12 22:09:39 +0900

    readwritelock files

--- include/linux/readwritelock.h
+++ include/linux/readwritelock.h
@@ -1,3 +1,6 @@
+#ifndef __READWRITELOCK_H__
+#define __READWRITELOCK_H__
+
 #include <linux/sched.h>
 #include <linux/list.h>
 #include <linux/spinlock.h>
@@ -11,11 +14,12 @@ static spinlock_t current_list_spinlock;
 static LIST_HEAD(waiting_lock_list);
 static spinlock_t waiting_list_spinlock;
 
-bool isInRange(int current, int degree, int range);
+bool isInRange(int x, int degree, int range);
 bool isCrossed(struct lock_struct *a, struct lock_struct *b);
 
-bool canLock(struct lock_struct *info);
-int lockProcess(int degree, int range, enum LockType type);
+bool canLock(struct lock_struct *info, struct list_head *temp_lock_list);
+int lockProcess(int degree, int range, int type);
+int wakeUp(void);
 int deleteProcess(int degree, int range, int type);
 
 asmlinkage int sys_rotlock_read(int degree, int range);
@@ -23,4 +27,4 @@ asmlinkage int sys_rotlock_write(int degree, int range);
 asmlinkage int sys_rotunlcok_read(int degree, int range);
 asmlinkage int sys_rotunlock_write(int degree, int range);
 
-void wakeUp(void);
+#endif
