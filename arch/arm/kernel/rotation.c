f1eb6d8cb7a33d4dcfe89d59fe62ca6142d18574
박 윤종 <yoonjong92@gmail.com>
2017-04-15 11:31:18 +0900

    code add

--- arch/arm/kernel/rotation.c
+++ arch/arm/kernel/rotation.c
@@ -20,7 +20,8 @@ int get_rotation(void)
 
 void exit_rotlock(struct task_struct *task)
 {
-	if(task->isReadWirteLock == false) return;	
+	if(task->isReadWirteLock == false) return;
+	prink(KERN_EMERG "is ReadWirteLock");
 	struct list_head *a;
 	struct lock_struct *alock;
 	struct lock_struct *n;
