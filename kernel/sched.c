2513ee3bd77b0f6148a0c95c1209c828111d6dcf
SeungyuLee <seunggyu.lee0619@gmail.com>
2017-05-14 20:23:14 +0900

    minor changes

--- kernel/sched.c
+++ kernel/sched.c
@@ -19,7 +19,8 @@ bool check_same_owner(struct task_struct *p){
 	return match;
 }
 
-asmlinkage int sys_sched_setweight(pid_t pid, int weight){
+asmlinkage int sys_sched_setweight(pid_t pid, int weight)
+{
 	struct task_struct *task = NULL;
 	struct pid *pid_struct = NULL;
 	struct rq *rq = NULL;
@@ -62,24 +63,24 @@ asmlinkage int sys_sched_setweight(pid_t pid, int weight){
 	else
 		task->wrr.weight = (unsigned int) weight;
 	
-	
-	spin_lock(&set_weight_lock);
-
 	rq = task_rq(task);
 	
-	// 
 	if( rq->curr == task || current == task)
 		task->wrr.time_slice = task->wrr.weight * 10;
 	else {
 		task->wrr.time_slice = task->wrr.weight * 10;
-		//task->wrr.time_left = task->wrr.time_slice / 10;
+		task->wrr.time_left = task->wrr.time_slice;
 	}
+	
+	spin_lock(&set_weight_lock);
 
 	wrr_rq = &rq->wrr;
 	wrr_rq->total_weight -= old_weight;
 	wrr_rq->total_weight += weight;
 	
 	spin_unlock(&set_weight_lock);
+
+	printk(KERN_DEBUG "SET_WEIGHT_INFO task pid %d is set as weight %d", task_pid_nr(current), task->wrr.weight);
 	return 0;
 }
 
@@ -105,6 +106,5 @@ asmlinkage int sys_sched_getweight(pid_t pid){
 		result = task->wrr.weight;
 	}
 
-	return result;
-	
+	return result;	
 }
