1c988e1295db1cda0ade1b034e89f1fe920557be
SeungyuLee <seunggyu.lee0619@gmail.com>
2017-05-24 23:04:20 +0900

    minor changes

--- arch/arm/kernel/gps.c
+++ arch/arm/kernel/gps.c
@@ -8,7 +8,7 @@
 static struct gps_kernel dev_location;
 static DEFINE_RWLOCK(dev_location_lock);
 
-asmlinkage int set_gps_location(struct gps_location __user *loc)
+asmlinkage int sys_set_gps_location(struct gps_location __user *loc)
 {
 	struct gps_location k_loc;
 
@@ -23,25 +23,25 @@ asmlinkage int set_gps_location(struct gps_location __user *loc)
 	if (copy_from_user(&k_loc, loc, sizeof(struct gps_location)))
 		return -EFAULT;
 
-	if (k_loc->lat_fractional < 0 || k_loc->lat_fractional > 999999)
+	if (k_loc.lat_fractional < 0 || k_loc.lat_fractional > 999999)
 		return -EINVAL;
 	
-	if (k_loc->lng_fractional < 0 || k_loc->lat_fractional > 999999)
+	if (k_loc.lng_fractional < 0 || k_loc.lat_fractional > 999999)
 		return -EINVAL;
 
-	if (k_loc->accuracy < 0)
+	if (k_loc.accuracy < 0)
 		return -EINVAL;
 
 	write_lock(&dev_location_lock);
-	dev_location.latitude = k_loc->lat_integer + k_loc->lat_fractional * 0.000001;
-	dev_location.longitude = k_loc->lng_integer + k_loc->lng_fractional * 0.000001;
-	dev_location.accuracy = k_lock->accuracy;
+	dev_location.latitude = k_loc.lat_integer + k_loc.lat_fractional * 0.000001;
+	dev_location.longitude = k_loc.lng_integer + k_loc.lng_fractional * 0.000001;
+	dev_location.accuracy = k_loc.accuracy;
 	write_unlock(&dev_location_lock);
 
 	return 0;
 }
 
-asmlinkage int get_gps_location(const char __user *pathname, struct gps_location __user *loc)
+asmlinkage int sys_get_gps_location(const char __user *pathname, struct gps_location __user *loc)
 {
 
 	return 0;
