81244adb19f118419372383c60adda0b3b7aac0a
SeungyuLee <seunggyu.lee0619@gmail.com>
2017-05-27 15:40:23 +0900

    get_gps_location system call implemented

--- arch/arm/kernel/gps.c
+++ arch/arm/kernel/gps.c
@@ -4,11 +4,12 @@
 #include <linux/uaccess.h>
 #include <linux/slab.h>
 #include <linux/gps.h>
+#include <linux/namei.h>
 
 static struct gps_location dev_location;
 static DEFINE_RWLOCK(dev_location_lock);
 
-struct gps_location get_gps_location()
+struct gps_location get_gps_location(void)
 {
 	struct	gps_location device_location;
 	read_lock(&dev_location_lock);
@@ -42,9 +43,9 @@ asmlinkage int sys_set_gps_location(struct gps_location __user *loc)
 		return -EINVAL;
 
 	write_lock(&dev_location_lock);
-	dev_location.lat_integer = k_loc.lat_integer 
+	dev_location.lat_integer = k_loc.lat_integer; 
 	dev_location.lat_fractional = k_loc.lat_fractional;
-	dev_location.lng_integer = k_loc.lng_integer 
+	dev_location.lng_integer = k_loc.lng_integer;
 	dev_location.lng_fractional = k_loc.lng_fractional;
 	dev_location.accuracy = k_loc.accuracy;
 	write_unlock(&dev_location_lock);
@@ -54,7 +55,52 @@ asmlinkage int sys_set_gps_location(struct gps_location __user *loc)
 
 asmlinkage int sys_get_gps_location(const char __user *pathname, struct gps_location __user *loc)
 {
+	char *k_pathname;
+	struct gps_location k_loc;
+	struct inode *inode;
+	struct path k_path;
+	int result;
 
+	int path_length = PATH_MAX + 1;
+	if(pathname == NULL || loc == NULL)
+		return -EINVAL;
+	k_pathname = kmalloc(path_length * sizeof(char), GFP_KERNEL);
+	if(k_pathname == NULL)
+		return -ENOMEM;
+	
+	result = strncpy_from_user(k_pathname, pathname, path_length);
+	
+	if(result <= 0 || result > path_length){
+		kfree(k_pathname);
+		return -EFAULT;
+	}
+
+	if(kern_path(pathname, LOOKUP_FOLLOW, &k_path)){
+		kfree(k_pathname);
+		return -EINVAL;
+	}
+
+	inode = k_path.dentry->d_inode;
+
+	if(!(S_IRUSR & inode->i_mode)){
+		kfree(k_pathname);
+		return -EACCES;
+	}
+
+
+	if(inode->i_op->get_gps_location==NULL){
+		kfree(k_pathname);
+		return -ENODEV;
+	}
+	else
+		inode->i_op->get_gps_location(inode, &k_loc);
+	
+	if(copy_to_user(loc, &k_loc, sizeof(struct gps_location))){
+		kfree(k_pathname);
+		return -EFAULT;
+	}
+
+	kfree(k_pathname);
 	return 0;
 }
 
